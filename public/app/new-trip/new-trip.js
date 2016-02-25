971727c59db0d8a4f7180cda6ae693fd4d01794c
steventsao <steventsao713@gmail.com>
2016-02-24 21:36:43 -0800

    Synchronize pins with all trips under user

--- public/app/new-trip/new-trip.js
+++ public/app/new-trip/new-trip.js
@@ -2,32 +2,41 @@ angular.module('app.new-trip', [])
 
 .controller('new-tripController', function($scope, $location, $window, Trips) {
 
-  $scope.trip = {};
   $scope.map;
   $scope.geocoder = new google.maps.Geocoder();
-  $scope.markers = [];
   $scope.destination;
+  var allTrips;
+  var coordinates = {};
 
-  // $scope.getTripData = function(destination, startDate){
-  //   Trips.newTrip(destination, startDate)
-  //     .then(function(response){
-  //       $scope.trip = response;
-  //       console.log($scope.trip)
-  //     })
-  // }
+  var createMarker = function (info) {
+    var marker = new google.maps.Marker({
+      map: $scope.map,
+      position: info.coordinates,
+      destination: info.destination
+    });
+  }
 
-  $scope.createTrip = function(destination, startDate) {
+  $scope.showTrips = function(user) {
+    Trips.allTrips(user)
+      .then(function(data) {
+        $scope.trips = data;
+        console.log($scope.trips);
+        $scope.trips.forEach(function(trip) {
+          if(trip.coordinates) createMarker(trip);
+      });
+
+    });
+
+
+  };
+  $scope.showTrips(Trips.user);
+
+
+  $scope.createTrip = function(destination, startDate, coordinates) {
   	// console.log($window.localStorage.getItem('com.tp'));
 
-    Trips.newTrip(destination, startDate);
+    Trips.newTrip(destination, startDate, coordinates);
 
-      // .then(function() {
-      //   var id = $scope.trip.data._id
-      //   console.log(id);
-      // // 	var tripID = response.data.trips.pop()._id;
-      // //   console.log(tripID);
-		    // // $location.path('/#/my-trip/' + tripID);
-      // })
   };
 
   var mapOptions = {
@@ -35,11 +44,19 @@ angular.module('app.new-trip', [])
     center: new google.maps.LatLng(37.09024, -95.712891),
     zoom: 5
   };
+
+  // create map 
   $scope.map = new google.maps.Map(document.getElementById("mapDiv"), mapOptions);
+
+
   $scope.map.addListener('click', function(e) {
     $.get("https://maps.googleapis.com/maps/api/geocode/json?latlng="+e.latLng.lat()+","+e.latLng.lng()+"&key=AIzaSyCXPMP0KsMOdfwehnmOUwu-W3VOK92CkwI", function(data) {
       console.log(data);
-      $scope.destination = data.results[1].formatted_address;
+      $scope.destination =  data.results[1].formatted_address;
+      coordinates.lat = data.results[0].geometry.location.lat;
+      coordinates.lng = data.results[0].geometry.location.lng;
+
+      console.log(coordinates);
 
       var req = {
         // FIXME: server side is not receiving trip information
@@ -48,17 +65,16 @@ angular.module('app.new-trip', [])
       }
 
       // @Date.now as a placeholder since server requires dates
-      Trips.newTrip($scope.destination, Date.now());
+      Trips.newTrip($scope.destination, Date.now(), coordinates);
   
       // place marker
       var marker = new google.maps.Marker({
         map: $scope.map,
         // FIXME: address does not update after dropping marker
-        draggable: true,
+        // draggable: true,
         position:data.results[0].geometry.location,
         animation: google.maps.Animation.DROP
       });
-        $scope.markers.push($scope.destination);
     });
    })
   $scope.geocodeAddress = function () {
@@ -73,7 +89,10 @@ angular.module('app.new-trip', [])
             position: results[0].geometry.location,
           });
           console.log(results)
+          coordinates.lat = results[0].geometry.location.lat();
+          coordinates.lng = results[0].geometry.location.lng();
           $scope.destination = results[0].formatted_address;
+          $scope.createTrip($scope.destination, Date.now(), coordinates)
           $scope.map.setZoom(6);
           $scope.map.panTo(marker.position)
         } else {
@@ -84,6 +103,8 @@ angular.module('app.new-trip', [])
 
   $scope.submitForm = function(){
     $scope.geocodeAddress();
-    $scope.createTrip($scope.destination, Date.now());
+    // $scope.createTrip($scope.destination, Date.now());
   }
+
+
 });
